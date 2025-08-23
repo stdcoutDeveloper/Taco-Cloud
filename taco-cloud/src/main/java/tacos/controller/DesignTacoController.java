af509d21d8795b0bc905e6b7c779232713afad79
stdcoutDeveloper <66522942+stdcoutDeveloper@users.noreply.github.com>
2025-08-23 23:28:25 +0700

    Processing form submission

--- taco-cloud/src/main/java/tacos/controller/DesignTacoController.java
+++ taco-cloud/src/main/java/tacos/controller/DesignTacoController.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.SessionAttributes;
 
@@ -67,6 +68,14 @@ public class DesignTacoController {
 	@GetMapping
 	public String showDesignForm() {
 		return "design";
+	}
+	
+	@PostMapping
+	public String processTaco(@ModelAttribute Taco taco, @ModelAttribute TacoOrder tacoOrder) {
+	    tacoOrder.addTaco(taco);
+	    log.info("Processing taco: {}", taco);
+
+	    return "redirect:/orders/current";
 	}
 
 	private Iterable<Ingredient> filterByType(List<Ingredient> ingredients, Type type) {
